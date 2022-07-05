1f4c14288989d9ced20c4838ca4c46b70817ce0f
kanzakirei <57294785+kanzakirei@users.noreply.github.com>
2022-07-06 08:56:40 +0900

    Update build.bat

--- html/build.bat
+++ html/build.bat
@@ -5,14 +5,14 @@ cd %~dp0
 for %%f in (*.html) do (
   echo %%f
   type nul > ../%%f
-  for %%t in (%%f) do (
+  for /f "delims=" %%t in (%%f) do (
     echo "%%t" | find "include" > nul
     if not ERRORLEVEL 1 (
       set key=%%t
       set key=!Key:*include(=!
-      set key=!key:);*=!
+      set key=!key:^);*=!
       echo include !key!
-      for %%i in (!key!) do (
+      for /f "delims=" %%i in (!key!) do (
         echo %%i >> ../%%f
       )
     ) else (
