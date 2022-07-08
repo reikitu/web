3b26de87b349e007cf852d7d835e1971de6af252
kanzakirei <57294785+kanzakirei@users.noreply.github.com>
2022-07-08 09:25:07 +0900

    Update buildV2.bat

--- html/buildV2.bat
+++ html/buildV2.bat
@@ -4,10 +4,29 @@ cd %~dp0 > nul
 
 for %%f in (*.html) do call :main_file %%f
 exit /b
+
 :main_file
 echo %1
-type nul>../%1
-for /f "delims=" %%t in (%1) do call :main_container %%t
+set MAIN_FILE_NAME=%1
+type nul>../%MAIN_FILE_NAME%
+for /f "delims=" %%t in (%MAIN_FILE_NAME%) do (
+  echo "%%t" | find "include" > nul
+  if not ERRORLEVEL 1 (
+    set key=%%t
+    setlocal enabledelayedexpansion
+    set key=!Key:^<^!--include =!
+    set key=!key:--^>=!
+    call :include_file !key!
+  ) else (
+  )
+)
+exit /b
+
+:include_file
+echo include %1
+for /f "delims=" %%i in (%1) do (
+    echo %%i>>../%MAIN_FILE_NAME%
+)
 exit /b
 
 :main_container
@@ -23,7 +42,7 @@ if not ERRORLEVEL 1 (
   )
   endlocal
 ) else (
-  set dirfix=%%t
+  set dirfix=%1
   setlocal enabledelayedexpansion
   set dirfix=!dirfix:../=!
   echo !dirfix!>>../%%f
