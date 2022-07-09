4e28122259ff71f8c73be8b7c49aa379bafadf9f
kanzakirei <kanzakirei.aqua@gmail.com>
2022-07-09 22:35:25 +0900

    バッチファイルの修正 ハンバーガー修正

--- html/build.bat
+++ html/build.bat
@@ -3,13 +3,14 @@ chcp 65001 > nul
 cd %~dp0 > nul
 
 for %%f in (*.html) do call :MainFile %%f
-del head.tmp body.tmp
+for %%f in (*.tmp) do del %%f
 exit /b
 
 :MainFile
 echo %1
 type nul>head.tmp
 type nul>body.tmp
+type nul>footer.tmp
 type nul>../%1
 for /f "delims=" %%t in (%1) do (
   echo "%%t" | find "include" > nul
@@ -24,11 +25,13 @@ for /f "delims=" %%t in (%1) do (
     set dirFix=%%t
     setlocal enabledelayedexpansion
     set dirFix=!dirFix:../=!
-    echo "!dirFix!" | find "</body>" > nul
-    if not ERRORLEVEL 1 type body.tmp>>../%1
     echo !dirFix!>>../%1
     echo "!dirFix!" | find "<head>" > nul
     if not ERRORLEVEL 1 type head.tmp>>../%1
+    echo "!dirFix!" | find "<body>" > nul
+    if not ERRORLEVEL 1 type body.tmp>>../%1
+    echo "!dirFix!" | find "<footer>" > nul
+    if not ERRORLEVEL 1 type footer.tmp>>../%1
     endlocal
     echo|set /p="."
   )
@@ -39,17 +42,28 @@ exit /b
 :IncludeFile
 echo #include %1
 for /f "delims=" %%i in (%1) do (
-  echo "%%i" | find "</head>" > nul
-  if not ERRORLEVEL 1 set isHead=0
-  echo "%%i" | find "</body>" > nul
-  if not ERRORLEVEL 1 set isBody=0
-  setlocal enabledelayedexpansion
-  if !isHead! equ 1 echo %%i>>head.tmp
-  if !isBody! equ 1 echo %%i>>body.tmp
-  endlocal
   echo "%%i" | find "<head>" > nul
-  if not ERRORLEVEL 1 set isHead=1
-  echo "%%i" | find "<body>" > nul
-  if not ERRORLEVEL 1 set isBody=1
+  if not ERRORLEVEL 1 (set tag=head) else (
+    echo "%%i" | find "</head>" > nul
+    if not ERRORLEVEL 1 (set tag=0) else (
+      echo "%%i" | find "<body>" > nul
+      if not ERRORLEVEL 1 (set tag=body) else (
+        echo "%%i" | find "</body>" > nul
+        if not ERRORLEVEL 1 (set tag=0) else (
+          echo "%%i" | find "<footer>" > nul
+          if not ERRORLEVEL 1 (set tag=footer) else (
+            echo "%%i" | find "</footer>" > nul
+            if not ERRORLEVEL 1 (set tag=0) else (
+              setlocal enabledelayedexpansion
+              if !tag! == head echo %%i>>head.tmp
+              if !tag! == body echo %%i>>body.tmp
+              if !tag! == footer echo %%i>>footer.tmp
+              endlocal
+            )
+          )
+        )
+      )
+    )
+  )
 )
 exit /b
