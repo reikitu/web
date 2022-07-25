73c9ba8632e8d537f7ea440a75079488c54457b0
kanzakirei <57294785+kanzakirei@users.noreply.github.com>
2022-07-25 20:43:38 +0900

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -2,6 +2,11 @@ function onClickedNavigationButton(element) {
   // 仮
   element.classList.toggle("active");
   let navigation = document.getElementById("navigation");
-  navigation.classList.toggle("fadeIn");
-  navigation.classList.toggle("fadeOut");
+  if(navigation.classList.length > 0) {
+    navigation.classList.toggle("fadeIn");
+    navigation.classList.toggle("fadeOut");
+  }
+  else {
+    navigation.classList.add("fadeIn");
+  }
 }
