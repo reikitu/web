f11c21db6a21c6115f8e403cdd148e1ff73e4a98
kanzakirei <kanzakirei.aqua@gmail.com>
2022-08-13 04:52:06 +0900

    メンバーの仮レイアウト

--- js/createMemberList.js
+++ js/createMemberList.js
@@ -1,48 +1,27 @@
 ﻿function createMemberList(_jsonText) {
 	let target = document.getElementById("memberList");
-	const titles = ["リーダー", "マネージャー", "コモンメンバー", "ビジター"];
-	let div = document.createElement("div");
-	div.classList.add("article");
-	target.appendChild(div);
-	let views = titles.map(title => createView(div, title));
 	_jsonText.forEach(data => {
-		let index = titles.indexOf(data.type);
-		if (index >= 0) {
-			createPanel(views[index], data.name, data.ImageID);
-			this._memberCount++;
-		}
-	});
-	views.forEach(view => {
-		if (view.children.length <= 0) {
-			createPanel(view, "この役職のメンバはいません。", "");
-		}
-	});
-}
+		let parallax = document.createElement("div");
+		parallax.classList.add("parallax");
+		target.appendChild(parallax);
 
-function createView(_parent, _title) {
-	let h2 = document.createElement("h2");
-	h2.classList.add("article-title");
-	h2.textContent = _title;
-	_parent.appendChild(h2);
-	let div = document.createElement("div");
-	div.classList.add("content");
-	_parent.appendChild(div);
-	return div;
-}
+		let h2 = document.createElement("h2");
+		h2.style.textAlign = "center";
+		h2.textContent = data.name;
+		parallax.appendChild(h2);
 
-function createPanel(_parent, _name, _imageID) {
-	let panel = document.createElement("div");
-	panel.classList.add("character_panel");
-	_parent.appendChild(panel);
-	let h3 = document.createElement("h3");
-	h3.textContent = _name;
-	panel.appendChild(h3);
-	if (_imageID) {
-		let imgDiv = document.createElement("div");
-		imgDiv.classList.add("character_image");
-		panel.appendChild(imgDiv);
+		let content = document.createElement("div");
+		content.classList.add("parallax_content");
+		parallax.appendChild(content);
+		let p = document.createElement("p");
+		p.style.textAlign = "left";
+		p.innerText = data.message;
+		content.appendChild(p);
+
+		let filename = data.filename ? data.filename : "no_image";
 		let img = document.createElement("img");
-		img.src = "https://drive.google.com/uc?export=view&id=" + _imageID + "&usp=sharing";
-		imgDiv.appendChild(img);
-	}
+		img.src = "img/member/" + filename + ".png";
+		//img.src = "https://drive.google.com/uc?export=view&id=" + _data.ImageID + "&usp=sharing";
+		parallax.appendChild(img);
+	});
 }
